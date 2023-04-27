f0c2d367a97ba997bf115cfe4306b648140eb1bb
sabir <sksabiruddin81@gmail.com>
2023-04-27 17:46:03 +0530

    submitting code-Thu Apr 27 2023 17:46:00 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
