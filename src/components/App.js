9120108193eba7f553a03abc9074f10c8a90a79f
sabir <sksabiruddin81@gmail.com>
2023-04-27 17:45:57 +0530

    submitting code-Thu Apr 27 2023 17:45:54 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,41 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+function App() {
+  const [value, setValue] = useState("");
+  function addValue(e) {
+    // console.log(e.target.innerHTML);
+    setValue((prev) => (prev += e.target.innerHTML));
+  }
+  function equal() {
+    setValue(eval(value));
+  }
+  function clearValue() {
+    setValue("");
+  }
+  return (
+    <div className="container">
+      <input type="text" value={value} />
+      <br />
+      <button onClick={clearValue}>c</button>
+      <button onClick={addValue}>/</button>
+      <button onClick={addValue}>*</button>
+      <button onClick={addValue}>-</button>
+      <br />
+      <button onClick={addValue}>7</button>
+      <button onClick={addValue}>8</button>
+      <button onClick={addValue}>9</button>
+      <button onClick={addValue}>+</button>
+      <br />
+      <button onClick={addValue}>1</button>
+      <button onClick={addValue}>2</button>
+      <button onClick={addValue}>3</button>
+      <button onClick={equal}>=</button>
+      <br />
+
+      <button onClick={addValue}>0</button>
+      <button onClick={addValue}>.</button>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
